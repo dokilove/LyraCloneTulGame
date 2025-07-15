30eb8adf75726f63a71ab8a00d2b5abee1d40d67
dokilove <dokilove2@gmail.com>
2025-07-15 18:44:04 +0900

    섹션 3. 라이라 입문 - 4. Module

---
+++ Source/TulGame/TulGame.cpp
@@ -0,0 +1,24 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TulGame.h"
+#include "Modules/ModuleManager.h"
+
+class FTulGameModule : public FDefaultGameModuleImpl
+{
+public:
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
+
+void FTulGameModule::StartupModule()
+{
+	FDefaultGameModuleImpl::StartupModule();
+	UE_LOG(LogTemp, Warning, TEXT("StartupModule!"));
+}
+
+void FTulGameModule::ShutdownModule()
+{
+	FDefaultGameModuleImpl::ShutdownModule();
+}
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FTulGameModule, TulGame, "TulGame" );
